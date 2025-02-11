dadbc3b180def8bc2802a5e503454196dc714fbd
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-02-11 15:50:17 -0300

    SQL raiz

--- src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2737;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2737.repositories.LawyerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
@@ -19,6 +24,17 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		List<LawyerMinProjection> list = repository.search1();
+		List<LawyerMinDTO> result1 = list.stream().map(x-> new LawyerMinDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** RESULTADO SQL RAIZ");
+		for (LawyerMinDTO obj : result1){
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
+
+
+
 		
 
 	}
